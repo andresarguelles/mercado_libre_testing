33c630a51188b860e5d10180386342c2af35ab9c
andresarguelles <aarguellesgonzalez@gmail.com>
2024-09-11 23:41:18 -0600

    Adding the python script file and README

---
+++ README.md
@@ -0,0 +1,33 @@
+# Mercado Libre Testing
+
+Script de prueba para busqueda automatizada de consolas "PlayStation 5" en articulos de Mercado Libre utilizando Selenium.
+
+## Requisitos
+
+1. **Python**: Asegúrate de tener Python instalado en tu sistema. Puedes descargarlo desde [python.org](https://www.python.org/).
+
+2. **Selenium**: Instala el paquete de Selenium utilizando pip:
+    ```sh
+    pip install selenium
+    ```
+
+3. **WebDriver**: Necesitas un WebDriver compatible con la versión de tu navegador. Por ejemplo, si usas Chrome, descarga `chromedriver`. Asegúrate de que el `chromedriver` esté en tu PATH o especifica su ubicación en el script.
+
+## Ejecución
+
+1. Clona este repositorio o descarga los archivos.
+
+2. Navega al directorio del proyecto:
+    ```sh
+    cd ruta/al/directorio
+    ```
+
+3. Ejecuta el script:
+    ```sh
+    python mercado_libre_testing.py
+    ```
+
+## Notas
+
+- Asegúrate de que el WebDriver que descargues sea compatible con tu versión y tipo de navegador.
+- De ser necesario modifica el script para adaptarlo a tus necesidades específicas de prueba.
---
+++ mercado_libre_testing.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+import chromedriver_binary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+brave_path = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+
+# # Configuramos chrome para que abra Brave
+options = webdriver.ChromeOptions()
+options.binary_location = brave_path
+
+# Inicializamos el driver con opciones personalizadas
+driver = webdriver.Chrome(options=options)
+
+try:
+    # Abrimos el sitio de Mercado Libre
+    driver.get("https://www.mercadolibre.com")
+
+    # Asignamos el boton de Mexico
+    mexico_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a[id='MX']"))
+    )
+    mexico_button.click()
+
+    # Buscamos el producto
+    search_box = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
+    search_box.send_keys("PlayStation 5")
+    search_box.send_keys(Keys.RETURN)
+
+    # Cerramos el banner de cookies si es que aparece
+    try:
+        cookie_banner = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cookie-consent-banner-opt-out__container button"))
+        )
+        cookie_banner.click()
+    except:
+        pass  
+
+    # Seleccionamos el filtro de "Nuevo"
+    nuevo_filter = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a.ui-search-link[title*='Nuevo']"))
+    )
+    nuevo_filter.click()
+
+    # Seleccionamos el filtro de ciudad "Distrito Federal"
+    ciudad_filter = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a.ui-search-link[title*='Distrito Federal']"))
+    )
+    ciudad_filter.click()
+
+    # Seleccionamos el filtro
+    filtro_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, ".andes-dropdown__trigger"))
+    )
+    filtro_button.click()
+
+    # Seleccionamos la opcion de precio de mayor a menor
+    de_mayor_a_menor_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.andes-list__item[data-key='price_desc']"))
+    )
+    de_mayor_a_menor_button.click()
+
+    #Obtenemos el titulo y precio con el tipo de moneda de los primeros 5 articulos
+    for i in range(5):
+        product = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, f".ui-search-layout__item:nth-child({i+1})"))
+        )
+        title = product.find_element(By.CSS_SELECTOR, "h2.ui-search-item__title").text
+        currency_simbol = product.find_element(By.CSS_SELECTOR, "span.andes-money-amount__currency-symbol").text
+        price = product.find_element(By.CSS_SELECTOR, "span.andes-money-amount__fraction").text
+        print(f"Producto {i+1}: {title} - Precio: {currency_simbol + price}")
+
+finally:
+    driver.quit()
